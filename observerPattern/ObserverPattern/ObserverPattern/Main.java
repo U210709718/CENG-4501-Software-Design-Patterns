0f1749adb898a7276d9097a9adddfd19bbae4b75
Seham Othman <210709718@posta.mu.edu.tr>
2025-11-07 16:32:55 +0300

    Update Main.java

--- observerPattern/ObserverPattern/ObserverPattern/Main.java
+++ observerPattern/ObserverPattern/ObserverPattern/Main.java
@@ -1,33 +1,21 @@
 package ObserverPattern.ObserverPattern;
-
-import java.util.Random;
-
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        //Create the data object (publisher / topic)
-        WeatherStation weatherStation = new WeatherStation();
+    public static void main(String[] args){
+        WeatherStation ws = new WeatherStation();
+        ws.add(new StatisticsDisplay());
+        ws.add(new CurrentConditionsDisplay());
+        ws.add(new ForecastDisplay());
 
-        //Create and register our displays (observers / subscribers)
-        CurrentConditionsDisplay currentConditionsDisplay =
-                new CurrentConditionsDisplay(weatherStation);
-        ForecastDisplayConditions forecastDisplay = new ForecastDisplayConditions(weatherStation);
+        System.out.println("weather station is having an update!");
+        ws.setMeasurements(20,90,80);
 
-        //Simulate updates
-        for (int i = 0; i < 5; i++) {
-            System.out.println("\n--- Update " + i + " ---");
 
-            int randomTemp = getRandomint(-50, 40);
-            int randomHumidity = getRandomint(0, 100);
-            int randompressure = getRandomint(950, 1050);
 
-            weatherStation.measurementsChanged(randomTemp, randomHumidity, randompressure);
 
-            Thread.sleep(1000);
-        }
-    }
 
-    private static int getRandomint(int min, int max) {
-        Random rand = new Random();
-        return rand.nextInt(max + 1 - min) + min;
+
+
+
+
     }
 }
