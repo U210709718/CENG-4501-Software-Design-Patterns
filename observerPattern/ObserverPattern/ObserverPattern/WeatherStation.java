3051c86aa11e73af9632f20ab8fac76df54aeeb1
Seham Othman <210709718@posta.mu.edu.tr>
2025-11-07 16:34:36 +0300

    Refactor WeatherStation to implement Observable interface

--- observerPattern/ObserverPattern/ObserverPattern/WeatherStation.java
+++ observerPattern/ObserverPattern/ObserverPattern/WeatherStation.java
@@ -1,37 +1,54 @@
 package ObserverPattern.ObserverPattern;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
-public class WeatherStation implements Subject {
-    private List<Observer> observers; //list of observers to maintain!
-    private float temperature;
-    private float humidity;
-    private float pressure;
+public class WeatherStation implements Observable {
+    private List<Observer> observers = new ArrayList<>();
+    private int temperature;
+    private int humidity;
+    private int pressure;
 
-    public WeatherStation() {
-        this.observers = new ArrayList<>();
-    }
     @Override
-    public void registerObserver(Observer o) {
+    public void add(Observer o) {
         observers.add(o);
+        
     }
 
     @Override
-    public void removeObserver(Observer o) {
+    public void remove(Observer o) {
         observers.remove(o);
     }
 
     @Override
-    public void notifyObservers() {
-        for (Observer observer : observers) {
-            observer.update(temperature, humidity, pressure);
+    public void broadcast() {
+        for (Observer o : observers) {
+            o.update();
         }
+        
     }
-    public void measurementsChanged(float temperature, float humidity, float pressure) {
+
+    public int getTemperature() {
+        return temperature;
+    }
+    public int getHumidity() {
+        return humidity;
+    }
+
+    public int getPressure() {
+        return pressure;
+    }
+    public void setMeasurements(int temperature, int humidity, int pressure){
         this.temperature = temperature;
         this.humidity = humidity;
         this.pressure = pressure;
-        notifyObservers(); //notify all observers of the change
+        System.out.println("measurements set! "+ temperature + "," + humidity + "," + pressure);
+
+        measurementsChanged();
     }
+    public void measurementsChanged(){
+        broadcast();
+    }
+
 }
+
