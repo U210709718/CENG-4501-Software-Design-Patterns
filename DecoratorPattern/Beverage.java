afb6873c5f19870ecf6e89c69c460c6516c4da14
Seham Othman <210709718@posta.mu.edu.tr>
2025-11-08 00:04:11 +0300

    Update Beverage.java

--- DecoratorPattern/Beverage.java
+++ DecoratorPattern/Beverage.java
@@ -1,3 +1,11 @@
-public class Beverage {
+public abstract class Beverage {
+    //Component being wrapped!
 
+    protected String description = "";
+
+
+    public String getDescription(){
+        return description;
+    }
+    public abstract int cost();
 }
